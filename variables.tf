f7d2ea86c9ed28734f37dcd57c123b44d3a4c2a6
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-11 16:35:59 -0300

    Update variables

--- variables.tf
+++ variables.tf
@@ -1,31 +1,27 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
+  type = string
 }
 
 variable "root_domain" {
-  description = "Root domain (e.g., example.com)"
-  type        = string
+  type = string
 }
 
 variable "subdomain" {
-  description = "Subdomain (e.g., www, app)"
-  type        = string
-}
-
-variable "target_domain" {
-  description = "The domain or service to point to (e.g., S3 website or CloudFront domain)"
-  type        = string
+  type = string
 }
 
 variable "target_record_type" {
-  description = "DNS record type to create (e.g., CNAME, A)"
   type        = string
+  description = "DNS record type (e.g., A, CNAME)"
   default     = "CNAME"
 }
 
+variable "target_domain" {
+  type        = string
+  description = "Target for the DNS record (e.g., d123.cloudfront.net)"
+}
+
 variable "ttl" {
-  description = "Time to live (TTL) for the DNS record"
-  type        = number
-  default     = 300
+  type    = number
+  default = 300
 }
