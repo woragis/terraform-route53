9df56205d94b9fd468c14c26972ee61bea433e4b
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-22 20:01:33 -0300

    Delete terraform

--- locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  fqdn = "${var.subdomain}.${var.root_domain}"
-}
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "lambda_url" {
-  value       = aws_apigatewayv2_api.http_api[0].api_endpoint
-  description = "Your Lambda app's public URL"
-}
